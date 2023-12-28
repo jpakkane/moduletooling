a0dc96e73709e429c7f04413816ff4a2281eb922
Jussi Pakkanen <jpakkane@gmail.com>
2023-12-28 14:56:55 +0200

    Create source files.

--- projectgenerator.py
+++ projectgenerator.py
@@ -8,6 +8,13 @@ p = argparse.ArgumentParser(prog='Module test project generator')
 p.add_argument('--source', help='Root dir for sources.', required=True)
 p.add_argument('--build', help='Root dir for build.', required=True)
 
+def create_sources(path, template):
+    num_sources = 10
+    for i in range(num_sources):
+        fname = path / (template % i)
+        with open(fname, 'w') as ofile:
+            ofile.write('This is a source file.\n')
+
 def generate():
     args = p.parse_args()
     if os.path.exists(args.source):
@@ -17,7 +24,9 @@ def generate():
     srcdir = pathlib.Path(args.source)
     builddir = pathlib.Path(args.build)
     ninjafile = builddir / 'build.ninja'
+    srcdir.mkdir()
     builddir.mkdir()
+    create_sources(srcdir, 'target0src%d.cpp')
     with open(ninjafile, "w") as n:
         n.write('ninja_required_version = 1.11.2\n\n')
         n.write('# Rules\n\n')
---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Jussi Pakkanen
+
+import os, sys, subprocess, shutil, pathlib
+
+srcdir = pathlib.Path('source')
+builddir = pathlib.Path('build')
+
+if srcdir.exists():
+    shutil.rmtree(srcdir)
+if builddir.exists():
+    shutil.rmtree(builddir)
+
+subprocess.check_call(['./projectgenerator.py',
+                       '--source',
+                       srcdir,
+                       '--build',
+                       builddir])
